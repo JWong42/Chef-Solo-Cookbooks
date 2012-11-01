c93de840bffc1c54f2c1d79c0336d2a12d1fdc73
Jakk Wong <jakk.wong@gmail.com>
2012-10-31 19:36:14 -0700

    Change unicorn default recipes file

--- cookbooks/unicorn/recipes/default.rb
+++ cookbooks/unicorn/recipes/default.rb
@@ -1,11 +1,1 @@
-gem_package "unicorn" do
-  version node[:unicorn][:version]
-end
-
-directory node[:unicorn][:config_path] do
-  mode 0755
-end
-
-cookbook_file "/usr/local/bin/unicornctl" do
-  mode 0755
-end
+gem_package "unicorn" 
